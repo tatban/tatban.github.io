01e62fee04883301d056b482bd95045991475e4d
Tathagata Bandyopadhyay <tathagata.banerjee15@rocketmail.com>
2024-09-30 00:39:17 +0200

    populating publications using java script loop

---
+++ pub.js
@@ -0,0 +1,43 @@
+function fetchPubs() {
+    let xhttp = new XMLHttpRequest();
+    var pubArr;
+    var doc_tab = document.getElementById('pubTAB');
+    doc_tab.innerHTML = null;
+    xhttp.open("GET", "https://tatban.github.io/pub.json", false)
+    xhttp.send(null)
+
+    pubArr = JSON.parse(xhttp.responseText);
+    //pubArr.sort(sortDate);
+
+    var resultHtml = "";
+    var currYear;
+    var conf_link;
+    var year = "";
+    pubArr.forEach(element => {
+        conf_link = element.published_in.conf_link
+        if (!conf_link){
+            conf_link = "#"
+        }
+
+        resultHtml += `
+        <tr>
+            <td class="pubRow">
+                <a href="${element.link}" class="papertitle">${element.title}</a><br>
+                ${element.authors} <br>
+                <em><a href="${conf_link}">${element.published_in.conf}</a></em>, ${element.year}
+            </td>
+        </tr>
+        `
+    });
+    console.log(resultHtml);
+    
+    console.log(doc_tab);
+    var tbody = document.createElement('tbody');
+    tbody.innerHTML = resultHtml;
+    doc_tab.appendChild(tbody);
+    //resultHtml += `
+    //</div>
+    //`
+
+    //return resultHtml;
+}
